b29fd226195bfd984b1deaf1d56d74bc9c6da973
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-03-05 10:04:46 -0800

    Print path followed on board along with minimum number of steps.

--- snl.cpp
+++ snl.cpp
@@ -19,44 +19,53 @@ int return_minimum_throws(int start_square, int end_square, int board_size, std:
 	for (int i = 1; i < board_size+1; ++i){
 		visited.insert({{i, false}});
 	}
-	std::queue<queue_entry> bfs_queue;
+	std::queue<queue_entry*> bfs_queue;
 	visited[start_square] = true;
 	queue_entry start = {start_square, 0, nullptr};
-    bfs_queue.push(start);
-    queue_entry current;
+    bfs_queue.push(&start);
+    queue_entry* current;
     while (!bfs_queue.empty())
     { 
         current = bfs_queue.front();
-        int board_position = current.square_number;
+        int board_position = current->square_number;
         if (board_position == end_square){
             break; 
         }
         bfs_queue.pop();
-        for (int j = current.square_number+1; j < current.square_number+7; ++j)
+        for (int j = current->square_number+1; j < current->square_number+7; ++j)
         {
             if (j <= board_size){
             	if (!visited[j]){
-            		queue_entry new_entry;
-            		new_entry.distance_from_start = current.distance_from_start+1;
+            		queue_entry* new_entry = (queue_entry*) malloc(sizeof(queue_entry*));
+            		new_entry->distance_from_start = current->distance_from_start+1;
                 	visited[j] = true;
                 	std::unordered_map<int, int>::const_iterator snakes_it = snakes->find(j);
 		    		std::unordered_map<int, int>::const_iterator ladders_it = ladders->find(j);
 					if (snakes_it != snakes->end()){
-						new_entry.square_number = snakes_it->second;
+						new_entry->square_number = snakes_it->second;
 					}
 					else if (ladders_it != ladders->end()){
-						new_entry.square_number = ladders_it->second;
+						new_entry->square_number = ladders_it->second;
 					}
 					else{
-						new_entry.square_number = j;
+						new_entry->square_number = j;
 					}
-					new_entry.next_node = nullptr;
+					new_entry->next_node = current;
 					bfs_queue.push(new_entry);
             	}
             }
         }
     }
-    return current.distance_from_start;
+    
+    int square;
+    queue_entry* iterate_thru = current;
+    while (iterate_thru->next_node != nullptr){
+    	std::cout << iterate_thru->square_number << " ";
+    	iterate_thru = iterate_thru->next_node;
+    }
+    std::cout << "\n";
+    
+    return current->distance_from_start;
 }
 
 int main(int argc, char** argv){
