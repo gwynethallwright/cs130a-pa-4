77c57f9373b7a665644293690a095c223222be41
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-03-04 16:27:36 -0800

    Create and print transition matrix. Not yet taking snakes and ladders into account.

--- snl.cpp
+++ snl.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
-#include <list>
 #include <vector>
-#include <array>
 #include <iterator>
 #include <sstream>
 #include <string>
-#include <stack>
-#include <queue>
+#include <unordered_map>
+
+std::vector<std::vector<int>> * create_transition_matrix(int board_size, std::unordered_map<int, int> snakes, std::unordered_map<int, int> ladders){
+	static std::vector<std::vector<int>> transition_matrix;
+	for (int board_position = 1; board_position != board_size+1; ++board_position){
+		std::vector<int> new_positions;
+		for (int num_rolled = 1; num_rolled != 7; ++num_rolled){
+			new_positions.push_back(std::min(board_position+num_rolled, board_size));
+		}
+		transition_matrix.push_back(new_positions);
+	}
+	return &transition_matrix;
+}
+
+void print_transition_matrix(std::vector<std::vector<int>> * transition_matrix){
+	for(std::vector<std::vector<int>>::iterator it = transition_matrix->begin(); it != transition_matrix->end(); ++it){
+		for(std::vector<int>::iterator it2 = (*it).begin(); it2 != (*it).end(); ++it2){
+			std::cout << *(it2) << " ";
+		}
+		std::cout << "\n";
+	}
+}
 
 int main(int argc, char** argv){
+	int board_size = 10;
+	std::unordered_map<int, int> snakes;
+	std::unordered_map<int, int> ladders;
+	std::vector<std::vector<int>> * transition_matrix = create_transition_matrix(board_size, snakes, ladders);
+	print_transition_matrix(transition_matrix);
 }
